60d85fb54b0cd3b1fca4c2137090d1f3aed7270f
luopan luopan <luopan@cbbdev01v.ats.shyc3.360es.cn>
2020-01-19 19:37:34 +0800

    compile tmp ok

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,26 +1,33 @@
 extern crate nom;
 use nom::{
-    bytes::complete::{is_not, take},
-    combinator::{opt, peek},
+    bytes::complete::{is_not},
+    combinator::{opt},
     IResult,
 };
+use std::os::raw::c_char;
+use std::ffi::{CString, CStr};
 
-#[no_mangle]
-pub extern fn peek_head(input: &str) -> IResult<&str, &str> {
-    let len = input.len();
-    if len > 50 {
-        return peek(take(50usize))(input);
-    }
-    peek(take(len))(input)
+#[repr(C)]
+pub struct PairStr{
+    left : *const c_char,
+    right : *const c_char,
 }
 
-#[no_mangle]
-pub extern fn space_find(input: &str) -> IResult<&str, &str> {
+pub fn space_find(input: &str) -> IResult<&str, &str> {
     let (char, found) = opt(is_not(" "))(input)?;
     if let Some(left) = found {
         return Ok((char.trim(), left));
     }
     Ok((input, ""))
+}
+
+#[no_mangle]
+pub extern fn space_find_export(input: &str) -> PairStr {
+    let (x, y) = space_find(input).unwrap();
+    PairStr{
+        left : CString::new(x).unwrap().into_raw(),
+        right : CString::new(y).unwrap().into_raw(),
+    }
 }
 
 
@@ -29,18 +36,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_peek_head() {
-        let input = "hello world";
-        let (old, head) = peek_head(input).unwrap();
-        assert_eq!(old, "hello world");
-        assert_eq!(head, "hello world");
-    }
-
-    #[test]
     fn test_space_find() {
         let input = "hello world";
-        let (char, left) = space_find(input).unwrap();
-        assert_eq!(char, "world");
-        assert_eq!(left, "hello");
+        let ret = space_find_export(input);
+        unsafe {
+            assert_eq!(CStr::from_ptr(ret.left).to_str().unwrap(), "world");
+            assert_eq!(CStr::from_ptr(ret.right).to_str().unwrap(), "hello");
+        }
     }
 }
